32e9999fcc3d19a6a4f436c3845f29afe816d097
Ubuntu <azureuser@djb-server1.uojksrmjd3pereubme5iah05aa.zx.internal.cloudapp.net>
2021-09-21 09:29:43 +0000

    trio task now working with docker-compose.yml

--- nginx/runNGinx.sh
+++ nginx/runNGinx.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --network=myNetwork --mount type=bind,source=$(pwd)/nginx.conf,target=/etc/nginx/nginx.conf -p 80:80 --name nginx nginx:alpine
+docker run -d --network myNetwork --mount type=bind,source=$(pwd)/nginx.conf,target=/etc/nginx/nginx.conf -p 80:80 --name nginx nginx:alpine
--- up.sh
+++ up.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+docker network create flask-app
 docker-compose up -d
